75afaaf2bc77ba09a6b8f7f8ac5e4257eafc24db
softtoday72 <demonhunter720525@hotmail.com>
2022-05-07 16:45:15 +0800

    add api error handler & delete restaurant api

--- routes/apis/modules/admin.js
+++ routes/apis/modules/admin.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../../controllers/apis/admin-controller')
+const { apiErrorHandler } = require('../../../middleware/error-handler')
 // const categoryController = require('../../../controllers/apis/category-controller')
 // const upload = require('../../../middleware/multer')
 
 // 實紀路由位置 '/api/admin/restaurants'
 router.get('/restaurants', adminController.getRestaurants)
-
+router.delete('/restaurants/:id', adminController.deleteRestaurant)
+router.use('/', apiErrorHandler)
 module.exports = router
